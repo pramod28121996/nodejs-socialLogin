6e07bf4ed0ec6188a2a89bd89aceda3640f38361
pramod <pramodvariyawork@gmail.com>
2024-06-17 11:59:55 +0530

    user register

---
+++ src/config/conn.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const mongourl = "mongodb://127.0.0.1:27017/user_db?directConnection=true";
+
+//database connenction
+mongoose.connect(mongourl)
+  .then(() => {
+    console.log("connecting success");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
---
+++ src/utils/endPoints.js
@@ -0,0 +1,6 @@
+const END_POINTS = {
+    SING_UP: '/sing_up',
+    LOGIN:"/login",
+}
+
+module.exports = END_POINTS
