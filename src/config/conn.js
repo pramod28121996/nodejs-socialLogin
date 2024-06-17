ef14103d5ab03a6ef0f4199984548f0243448f07
pramod <pramodvariyawork@gmail.com>
2024-06-17 14:53:52 +0530

    login implement

--- src/config/conn.js
+++ src/config/conn.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const mongourl = "mongodb://127.0.0.1:27017/user_db?directConnection=true";
 
 //database connenction
-mongoose.connect(mongourl)
+mongoose
+  .connect(mongourl)
   .then(() => {
     console.log("connecting success");
   })
--- src/utils/endPoints.js
+++ src/utils/endPoints.js
@@ -1,6 +1,7 @@
 const END_POINTS = {
     SING_UP: '/sing_up',
     LOGIN:"/login",
+    ME:"/me"
 }
 
 module.exports = END_POINTS
